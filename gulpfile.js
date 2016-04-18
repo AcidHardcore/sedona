e3996c736c2e9a297f3742be489d1871cf35fe83
AcidHardcore <vitcore@mail.ru>
2016-04-18 14:23:39 +0300

    gulp update css task

--- gulpfile.js
+++ gulpfile.js
@@ -2,42 +2,47 @@
 
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
-    cleanCSS = require('gulp-clean-css'),
+    cleancss = require('gulp-clean-css'),
     rename = require('gulp-rename'),
-    prefixer = require('gulp-autoprefixer'),
+    autoprefixer = require('gulp-autoprefixer'),
     less = require('gulp-less'),
     sourcemaps = require('gulp-sourcemaps'),
     browserSync = require('browser-sync').create(),
     concat = require('gulp-concat'),
+    notify = require('gulp-notify'),
+    gulpIf = require('gulp-if'),
+    debug = require('gulp-debug'),
+    mqpacker = require('css-mqpacker'),
+    postcss = require('gulp-postcss'),
     csscomb = require('gulp-csscomb');
-    // livereload = require('gulp-livereload');
-    // rigger = require('gulp-rigger'),
-    // imagemin = require('gulp-imagemin'),
-    // pngquant = require('imagemin-pngquant'),
-    // rimraf = require('rimraf'),
+// rigger = require('gulp-rigger'),
+// imagemin = require('gulp-imagemin'),
+// pngquant = require('imagemin-pngquant'),
+// rimraf = require('rimraf'),
 
-gulp.task('comb', function () {
-    return gulp.src('./less/**/*.*')
-        .pipe(csscomb())
-        .pipe(gulp.dest('./less/**/*.*'))
-});
+// Запуск `NODE_ENV=production npm start [задача]` приведет к сборке без sourcemaps
+const isDev = !process.env.NODE_ENV || process.env.NODE_ENV == 'dev';
 
+// Компиляция LESS
 gulp.task('css', function () {
     return gulp.src('./less/style.less')
-        .pipe(sourcemaps.init())
-        .pipe(csscomb())
+        .pipe(gulpIf(isDev, sourcemaps.init()))
+        .pipe(debug({title: "LESS:"}))
         .pipe(less())
-        .pipe(prefixer())
-        .pipe(cleanCSS())
+        .on('error', notify.onError(function (err) {
+            return {
+                title: 'Styles compilation error',
+                message: err.message
+            }
+        }))
+        .pipe(autoprefixer({browsers: ['last 2 version']}))
+        .pipe(csscomb())
+        .pipe(gulpIf(!isDev, cleancss()))
         .pipe(rename({suffix: '.min'}))
-        .pipe(sourcemaps.write())
-        .pipe(gulp.dest('./css/min/'));
+        .pipe(gulpIf(isDev, sourcemaps.write()))
+        .pipe(gulp.dest('./css/'));
 });
 
-// gulp.task('watch', function () {
-//     // gulp.watch('./less/components/*.less', ['less']);
-//     gulp.watch('./less/components/*.less', ['css']);
-// });
 gulp.task('watch', function () {
     gulp.watch('./less/components/*.less', gulp.series('css'));
 });
@@ -49,9 +54,9 @@ gulp.task('serve', function () {
             index: "index.html"
         }
     });
-    browserSync.watch('css/min/*.*').on('change', browserSync.reload);
+    browserSync.watch('css/*.*').on('change', browserSync.reload);
 });
 
-gulp.task( 'dev',
+gulp.task('dev',
     gulp.series('css', gulp.parallel('watch', 'serve'))
 );
