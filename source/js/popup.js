64df083b026838d914f20f3714b6dcdcc6fab502
AcidHardcore <vitcore@mail.ru>
2016-05-05 14:26:44 +0300

    some markup amends for js

--- source/js/popup.js
+++ source/js/popup.js
@@ -4,6 +4,10 @@ var popup = document.querySelector('.page-nav__list');
 var close = popup.querySelector('.page-nav__close');
  var form = document.querySelector('.search');
 var mapOpen = document.querySelector(".btn--search-collapse");
+var adult = document.querySelector('.search__data-adult-inner');
+var adultPlus = adult.querySelector('.search__plus');
+var adultInput = adult.querySelector('.search__input-number');
+var adultMinus = adult.querySelector('.search__minus');
 
 //menu and form hide on load page
 window.onload = function () {
@@ -25,3 +29,12 @@ mapOpen.addEventListener('click', function (event) {
    event.preventDefault();
     form.classList.toggle('search--hide-js');
 });
+//plus, minus buttons
+// adultPlus.addEventListener('click', function (event) {
+//    event.preventDefault();
+//      adultInput.value = parseInt(adultInput.value) + 1;
+// });
+// adultMinus.addEventListener('click', function (event) {
+//     event.preventDefault();
+//     adultInput.value = parseInt(adultInput.value) - 1;
+// });
