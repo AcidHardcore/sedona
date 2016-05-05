b7eabeae06d12e48de626c47080ac7c148049828
AcidHardcore <vitcore@mail.ru>
2016-05-05 16:35:38 +0300

    add picture and picturefill

--- source/js/popup.js
+++ source/js/popup.js
@@ -5,9 +5,13 @@ var close = popup.querySelector('.page-nav__close');
  var form = document.querySelector('.search');
 var mapOpen = document.querySelector(".btn--search-collapse");
 var adult = document.querySelector('.search__data-adult-inner');
-var adultPlus = adult.querySelector('.search__plus');
+var adultPlus = adult.querySelector('.btn--plus');
 var adultInput = adult.querySelector('.search__input-number');
-var adultMinus = adult.querySelector('.search__minus');
+var adultMinus = adult.querySelector('.btn--minus');
+var children = document.querySelector('.search__data-children-inner');
+var childrenPlus = children.querySelector('.btn--plus');
+var childrenInput = children.querySelector('.search__input-number');
+var childrenMinus = children.querySelector('.btn--minus');
 
 //menu and form hide on load page
 window.onload = function () {
@@ -29,12 +33,25 @@ mapOpen.addEventListener('click', function (event) {
    event.preventDefault();
     form.classList.toggle('search--hide-js');
 });
-//plus, minus buttons
-// adultPlus.addEventListener('click', function (event) {
-//    event.preventDefault();
-//      adultInput.value = parseInt(adultInput.value) + 1;
-// });
-// adultMinus.addEventListener('click', function (event) {
-//     event.preventDefault();
-//     adultInput.value = parseInt(adultInput.value) - 1;
-// });
+//plus, minus buttons adult
+adultPlus.addEventListener('click', function (event) {
+   event.preventDefault();
+     adultInput.value = parseInt(adultInput.value) + 1;
+});
+adultMinus.addEventListener('click', function (event) {
+    event.preventDefault();
+    if (adultInput.value > 0) {
+        adultInput.value = parseInt(adultInput.value) - 1;
+    }
+});
+//plus, minus buttons children
+childrenPlus.addEventListener('click', function (event) {
+    event.preventDefault();
+    childrenInput.value = parseInt(childrenInput.value) + 1;
+});
+childrenMinus.addEventListener('click', function (event) {
+    event.preventDefault();
+    if (childrenInput.value > 0) {
+        childrenInput.value = parseInt(childrenInput.value) - 1;
+    }
+});
